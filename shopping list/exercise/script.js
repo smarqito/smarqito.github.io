c5ea126d4c0715df0b2a2280b87b8f8b72c69d50
Marco António Sousa <48361688+smarqito@users.noreply.github.com>
2019-03-17 11:17:06 +0000

    shopping list

---
+++ shopping list/exercise/script.js
@@ -0,0 +1,57 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.getElementsByTagName("li");
+var buttonDelete = document.getElementsByName("liDelete");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	var liNewButton = document.createElement("button");
+	liNewButton.setAttribute("name", "liDelete");
+	liNewButton.appendChild(document.createTextNode("Delete"));
+	li.appendChild(liNewButton);
+	checkClick()
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function liClick() {
+	this.classList.toggle("done");
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+function checkClick() {
+	for (var d = 0; d < buttonDelete.length; d++) {
+	buttonDelete[d].addEventListener("click", liDelete);
+	}
+	for (var i = 0; i < li.length ; i++) {
+	li[i].addEventListener("click",	liClick);
+	}
+}
+
+window.onload = function() {
+	checkClick();
+}
+function liDelete() {
+	console.log(this.parentElement.parentElement.removeChild(this.parentElement));
+}
